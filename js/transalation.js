92994096ea1758f28b5c92334da0a5017ec23dc9
Elyrah408 <e.rahimi2019@gmail.com>
2024-11-26 08:29:13 +0100

    Add files via upload

---
+++ js/transalation.js
@@ -0,0 +1,22 @@
+const transalation = {  fr: {select:"Langue",
+    hometext1:"Choisissez le nombre de couleurs qu'il vous faut", 
+    header1:"Daltonia",
+    submit:"convert",printhead:"les color que t'as choisi"
+   ,butondownload:"Télécharger le PDF",colorButton:"Choisissez le nombre de couleurs qu'il vous faut",
+redirect:"page D'accueil"},
+
+
+en: {select:"Language",hometext1:"Choose the number of colors that you need",
+   header1:"Daltonia", submit:"convert",printhead:"Selected Colors list",butondownload:"Download PDF",colorButton:"Choose the number of colors that you need",redirect:"Home page"},
+   
+ar: {select:"لغة",hometext1:"اختر عدد الألوان التي تحتاجها",
+   header1:"دالتونيا", submit:"يتحول",printhead:"قائمة الألوان المختارة",butondownload:"تحميل قوات الدفاع الشعبي",colorButton:"اختر عدد الألوان التي تحتاجها",redirect:"الصفحة الرئيسية"},
+
+   ch: {select:"语言",hometext1:"选择您需要的颜色数量",
+      header1:"达尔顿尼亚", submit:"转变",printhead:"选定的颜色列表",butondownload:"下载PDF",colorButton:"选择您需要的颜色数量",redirect:"主页"}
+};
+
+localStorage.setItem("transalation",transalation);
+
+
+
